cffe90619ea8ee088b4cdbf745844d4868b248f4
Steven Keuchel <steven.keuchel@gmail.com>
2013-04-16 17:02:47 +0200

    Get rid of the Bind case in EffM.

--- Eff.hs
+++ Eff.hs
@@ -1,9 +1,7 @@
 
 {-# LANGUAGE DataKinds, KindSignatures, GADTs,
              MultiParamTypeClasses, FlexibleInstances,
-             PolyKinds, TypeFamilies, TypeOperators, RebindableSyntax #-}
-
-import Prelude (Int, fromInteger, (+), undefined, Show(..), Bool(..))
+             PolyKinds, TypeFamilies, TypeOperators #-}
 
 type family Res (e :: * -> *) :: *
 
@@ -16,18 +14,16 @@ data Elem :: k -> [k] -> * where
 
 data EffM (m :: * -> *) :: [* -> *] -> * -> * where
   Return    :: a -> EffM m es a
-  Bind      :: EffM m es a -> (a -> EffM m es b) -> EffM m es b
-  New       :: (Handler e m) =>
-               Res e -> EffM m (e ': es) a -> EffM m es a
-  MkEffectP :: Elem e es -> e t -> EffM m es t
+  New       :: Handler e m => Res e -> EffM m (e ': es) a ->
+               (a -> EffM m es b) -> EffM m es b
+  MkEffectP :: Elem e es -> e a ->
+               (a -> EffM m es b) -> EffM m es b
 
-return :: a -> EffM m es a
-return = Return
-
-(>>=) = Bind
-(>>) a f = a >>= \_ -> f
-
-fail = undefined
+instance Monad (EffM m es) where
+  return                  = Return
+  Return a          >>= g = g a
+  New r e f         >>= g = New r e (\a -> f a >>= g)
+  MkEffectP p eff f >>= g = MkEffectP p eff (\a -> f a >>= g)
 
 -- Running
 data Id a = Id { fromId :: a }
@@ -41,10 +37,10 @@ execEff (Cons res env) Here eff k = handle res eff (\res' v -> k (Cons res' env)
 execEff (Cons res env) (There i) eff k = execEff env i eff (\env' -> k (Cons res env'))
 
 effInt :: Env m es -> EffM m es a -> (Env m es -> a -> m b) -> m b
-effInt env (Return a)      k = k env a
-effInt env (Bind prog c)   k = effInt env prog (\env' a -> effInt env' (c a) k)
-effInt env (New r prog)    k = effInt (Cons r env) prog (\(Cons _ env') -> k env')
-effInt env (MkEffectP p e) k = execEff env p e k
+effInt env (Return a)        k = k env a
+effInt env (New r prog c)    k = effInt (Cons r env) prog
+                                   (\(Cons _ env') v -> effInt env' (c v) k)
+effInt env (MkEffectP p e c) k = execEff env p e (\env' v -> effInt env' (c v) k)
 
 runPure :: Env Id es -> EffM Id es a -> a
 runPure env prog = fromId (effInt env prog (\env' -> Id))
@@ -61,10 +57,10 @@ instance Handler (State s) m where
   handle st (Put st') k = k st' ()
 
 get :: EffM m '[State s] s
-get = MkEffectP Here Get
+get = MkEffectP Here Get Return
 
 put :: s -> EffM m '[State s] ()
-put s = MkEffectP Here (Put s)
+put s = MkEffectP Here (Put s) Return
 
 data Tree a = Leaf | Node a (Tree a) (Tree a)
   deriving Show
@@ -84,26 +80,12 @@ test = Node True (Node False Leaf Leaf) Leaf
 runTest :: Tree Int
 runTest = runPure (Cons 5 Nil) (tag test)
 
-{-
-class Handler e m where
-  handle :: res -> (e res res') -> (res' -> m a) -> m a
 
-class EFFECT (e :: * -> * -> *) where
-  type Res
+data Free f a where
+  Pure   :: a -> Free f a
+  Impure :: f (Free f a) -> Free f a
 
-data State :: * -> * -> * where
-  Get :: State a a
-  Put :: b -> State a b
-
-instance Handler State m where
-  handle st Get     k = k st
-  handle st (Put n) k = k n
-
-
-data EffM (m :: * -> *) :: [* -> * -> *] -> [* -> * -> *] -> * -> * where
-  Return :: a -> EffM m es es a
-  Bind   :: EffM m es es' a -> (a -> EffM m es' es'' b) -> EffM m es es'' b
-  New    :: (EFFECT e, Handler e m) =>
-            Res -> EffM m (e ': es) (e ': es') a -> EffM m es es' a
-
--}
+instance Functor f => Monad (Free f) where
+  return         = Pure
+  Pure   a >>= f = f a
+  Impure x >>= f = Impure (fmap (>>=f) x)
